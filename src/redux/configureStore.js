4ed4ae9e2471dfcac6b104ec78eabd2d312cf8fb
Tata Kowshik <tatakowshik@gmail.com>
2020-06-12 20:28:11 +0530

    Redux Thunk

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,10 @@
-import {createStore, combineReducers} from 'redux';
+import {createStore, combineReducers, applyMiddleware } from 'redux';
 import { Dishes } from './dishes';
 import { Comments } from './comments';
 import { Promotions } from './promotions';
 import { Leaders } from './leaders';
-
+import thunk from 'redux-thunk'; //thunk-middleware
+import logger from 'redux-logger';//logging
 export const ConfigureStore = () => {
     //combined reducer using combineReducers
     const store = createStore(
@@ -12,7 +13,8 @@ export const ConfigureStore = () => {
             comments: Comments,
             promotions: Promotions,
             leaders: Leaders
-        })
+        }),
+        applyMiddleware(thunk, logger)//enhancer
     );
 
     return store;
